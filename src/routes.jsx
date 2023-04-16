bc6ff1e0a55b0ebc59c4fc9d4f3c6d7eab416b53
gtod8010 <gtod8010@naver.com>
2023-04-16 17:50:23 +0900

    feat: 필요없는 파일 삭제 및 메인 경로 works로 변경

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,19 +1,9 @@
 import {
-  HomeIcon,
-  UserCircleIcon,
-  TableCellsIcon,
-  BellIcon,
-  ArrowRightOnRectangleIcon,
-  UserPlusIcon,
   ComputerDesktopIcon
 } from "@heroicons/react/24/solid";
-import { Home, Profile, Notifications } from "@/pages/dashboard";
-import Workers from "@/pages/dashboard/workers/Main";
-import Tasks from "@/pages/dashboard/Tasks/Main";
-import WorkerDetail from "@/pages/dashboard/machines/Detail";
-// import ConnectOption from "@/pages/dashboard/order/connectOption";
 
-import { SignIn, SignUp } from "@/pages/auth";
+import Workers from "@/pages/dashboard/workers/Main";
+import Processes from "@/pages/dashboard/Processes/Main";
 
 const icon = {
   className: "w-5 h-5 text-inherit",
@@ -23,24 +13,7 @@ export const routes = [
   {
     layout: "dashboard",
     pages: [
-      // {
-      //   icon: <HomeIcon {...icon} />,
-      //   name: "dashboard",
-      //   path: "/home",
-      //   element: <Home />,
-      // },
-      // {
-      //   icon: <UserCircleIcon {...icon} />,
-      //   name: "profile",
-      //   path: "/profile",
-      //   element: <Profile />,
-      // },
-      // {
-      //   icon: <BellIcon {...icon} />,
-      //   name: "notifactions",
-      //   path: "/notifactions",
-      //   element: <Notifications />,
-      // },
+
       {
         icon: <ComputerDesktopIcon {...icon} />,
         name: "Workers",
@@ -49,42 +22,12 @@ export const routes = [
       },
       {
         icon: <ComputerDesktopIcon {...icon} />,
-        name: "Task List",
-        path: "/Tasks",
-        element: <Tasks />,
+        name: "Process List",
+        path: "/Processes",
+        element: <Processes />,
       },
-      // {
-      //   icon: <ComputerDesktopIcon {...icon} />,
-      //   name: "details",
-      //   path: "/worker/:id",
-      //   element: <WorkerDetail />,
-      // },
-      // {
-      //   icon: <ComputerDesktopIcon {...icon} />,
-      //   name: "connect",
-      //   path: "/connect",
-      //   element: <ConnectOption />,
-      // },
     ],
   },
-  // {
-  //   title: "auth pages",
-  //   layout: "auth",
-  //   pages: [
-  //     {
-  //       icon: <ArrowRightOnRectangleIcon {...icon} />,
-  //       name: "sign in",
-  //       path: "/sign-in",
-  //       element: <SignIn />,
-  //     },
-  //     {
-  //       icon: <UserPlusIcon {...icon} />,
-  //       name: "sign up",
-  //       path: "/sign-up",
-  //       element: <SignUp />,
-  //     },
-  //   ],
-  // },
 ];
 
 export default routes;
