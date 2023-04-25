e2aaf5750a126e8c442171628d9f8749f4883ce3
Dwight.k <donghyun.k@stryx.co.kr>
2023-04-26 03:08:46 +0900

    feat: 여러 작업 진행

--- src/pages/dashboard/processes/Detail/ClientList.jsx
+++ src/pages/dashboard/processes/Detail/ClientList.jsx
@@ -1,13 +1,81 @@
-import React, {useCallback, useState, useEffect} from 'react'
+import React, { useCallback, useState, useEffect } from 'react'
 import { Chip, Button, Tooltip, Typography } from '@material-tailwind/react'
 import {
   ArrowPathIcon
 } from "@heroicons/react/24/solid";
 
+const serverUri = process.env.NODE_ENV === 'development' ? 'http://141.164.51.175:225' : 'https://macro-server.com';
+
 
 export default function ClientList({ process, setProcess, processId, getDetail }) {
   const [refreshCount, setRefreshCount] = useState(10)
+  const [currentStatus, setCurrentStatus] = useState(null);
+  const [isShowClientList, setIsShowClientList] = useState(false);
 
+  const handleClientIdClick = (status) => {
+    if(status === currentStatus) {
+      setIsShowClientList(false);
+      setCurrentStatus(null);
+      return
+    }
+    setCurrentStatus(status);
+    setIsShowClientList(true);
+  };
+
+  const getStatusCount = (status) => {
+    const count = process?.clients?.child?.filter((child) => child.status === status).length;
+    return count || 0;
+  };
+
+  const handleRefreshClick = useCallback(() => {
+    getDetail(processId);
+    setRefreshCount(10);
+  }, []);
+
+  const handleFailRestart = async () => {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const hour = String(currentDate.getHours()).padStart(2, '0');
+    const minute = String(currentDate.getMinutes()).padStart(2, '0');
+    const second = String(currentDate.getSeconds()).padStart(2, '0');
+
+    const formattedDate = `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+
+    let failClients = process?.clients?.child?.filter(client => client.status === 'FAIL').map(client => client);
+
+    failClients.forEach(async (client) => {
+      const body = {
+        clientId: client.clientId,
+        title: process?.clients.title,
+        keyword: process?.clients.keyword,
+        target: "FAIL",
+        method: "findKeyword",
+        prePageDown: 0,
+        execurationAt: formattedDate,
+        endDate: formattedDate
+      };
+
+      try {
+        const fetchRes = await fetch(`${serverUri}/api/client/${client.clientId}`, {
+          method: 'PATCH',
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(body)
+        });
+
+        const responseData = await fetchRes.json();
+
+        if (responseData && responseData.code === 200) {
+          console.log('성공');
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    });
+  };
 
   useEffect(() => {
     const reduceCount = setInterval(() => {
@@ -27,58 +95,72 @@ export default function ClientList({ process, setProcess, processId, getDetail }
     }
   }, [refreshCount, getDetail, processId])
 
-  const handleChipClick = useCallback(() =>{
-    getDetail(processId);
-    setRefreshCount(10);
-  }, []);
 
   return (
     <div>
       <div className="flex gap-3  border p-4 rounded-md">
         <div className="flex-1">
-        <Typography variant="small" className="font-normal text-blue-gray-500 mb-4">
-              클라이언트 상태
-            </Typography>
+          <Typography variant="small" className="font-normal text-blue-gray-500 mb-4">
+            클라이언트 상태
+          </Typography>
           <div className="flex gap-4 items-center">
-            <Chip color='cyan' value='대기중' />
-            <Chip color='blue' value='동작중' />
-            <Chip color='green' value='시청중' />
-            <Chip color='yellow' value='연결에러' />
-            <Chip color='gray' value='재실행대기' />
-            <Chip color='red' value='찾기실패' />
+            <Chip
+              color={getStatusCount('IDLE') === 0 ? 'gray' : 'cyan'}
+              value={`대기중 ${getStatusCount('IDLE')} 명`}
+              onClick={() => handleClientIdClick('IDLE')}
+            />
+            <Chip
+              color={getStatusCount('RUNNING') === 0 ? 'gray' : 'blue'}
+              value={`동작중 ${getStatusCount('RUNNING')} 명`}
+              onClick={() => handleClientIdClick('RUNNING')}
+            />
+            <Chip
+              color={getStatusCount('WATCHING') === 0 ? 'gray' : 'green'}
+              value={`시청중 ${getStatusCount('WATCHING')}명`}
+              onClick={() => handleClientIdClick('IDLE')}
+            />
+            <Chip
+              color={getStatusCount('ERROR') === 0 ? 'gray' : 'yellow'}
+              value={`연결에러 ${getStatusCount('ERROR')} 명`}
+              onClick={() => handleClientIdClick('IDLE')}
+            />
+            <Chip
+              color={getStatusCount('TIMEOUT') === 0 ? 'gray' : 'gray'}
+              value={`재실행대기 ${getStatusCount('TIMEOUT')} 명`}
+              onClick={() => handleClientIdClick('IDLE')}
+            />
+            <Chip
+              color={getStatusCount('FAIL') === 0 ? 'gray' : 'red'}
+              value={`찾기실패 ${getStatusCount('FAIL')} 명`}
+              onClick={() => handleClientIdClick('IDLE')}
+            />
           </div>
         </div>
         <div className="flex-1">
           <div className='flex flex-1 gap-2 justify-end'>
             <Tooltip content="클릭 시 클라이언트들의 상태를 즉시 새로고침 합니다.">
-              <Button color ='indigo' onClick={handleChipClick} className="flex items-center">
+              <Button color='indigo' onClick={handleRefreshClick} className="flex items-center">
                 <ArrowPathIcon className="w-5 h-5 text-inherit" />
                 <span>{`${refreshCount} 초 전`}</span>
               </Button>
             </Tooltip>
             <Tooltip content="문자열 찾기에 실패한 모든 클라이언트에게 수동 재실행 명령을 보내줍니다.">
-              <Button color = 'teal'>수동 재실행</Button>
+              <Button color='teal' onClick={handleFailRestart} disabled>수동 재실행 </Button>
             </Tooltip>
           </div>
         </div>
       </div>
-      {/* <div className="flex gap-3  border p-4 rounded-md">
-        <div className="flex-1">
-          <div className="flex gap-4 items-center ">
-            <Typography variant="h6" className="flex gap-4 items-center">
-              {`커맨드 : ${process.clients?.clientsCount} 개`}
-            </Typography>
-          </div>
+      {isShowClientList && (
+        <div className="flex-1 flex gap-1">
+          {process?.clients?.child?.filter(client => client.status === currentStatus).map(client => (
+            <Chip
+              key={client.clientId}
+              color="cyan"
+              value={client.clientId}
+            />
+          ))}
         </div>
-        <div className="flex-1">
-          <div className="flex gap-1 items-center">
-          </div>
-        </div>
-        <div className="flex-1">
-          <div className='flex flex-1 justify-end'>
-          </div>
-        </div>
-      </div> */}
+      )}
     </div>
   )
 }
--- src/pages/dashboard/processes/Detail/index.jsx
+++ src/pages/dashboard/processes/Detail/index.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import {
   Card,
   CardHeader,
   CardBody,
   Typography,
-  Button
+  Button,
+  Input
 } from "@material-tailwind/react";
 
 import ClientList from '@/pages/dashboard/processes/Detail/ClientList';
@@ -13,8 +14,19 @@ import ClientList from '@/pages/dashboard/processes/Detail/ClientList';
 const serverUri = process.env.NODE_ENV === 'development' ? 'http://141.164.51.175:225' : 'https://macro-server.com';
 
 export default function Detail() {
-  const [process, setProcess] = React.useState([])
-  const [processDetail, setProcessDetail] = React.useState({
+  const [process, setProcess] = useState([])
+  const [isEditing, setIsEditing] = useState(false);
+  const location = useLocation()
+  const processId = location.pathname.slice(location.pathname.lastIndexOf('/') + 1)
+  const [processDetail, setProcessDetail] = useState({
+    keyword: '',
+    title: '',
+    createdAt: '',
+    endDate: '',
+    clienstCount: 0,
+
+  })
+  const [updateProcessDetail, setUpdateProcessDetail] = useState({
     keyword: '',
     title: '',
     createdAt: '',
@@ -23,20 +35,10 @@ export default function Detail() {
 
   })
 
-  const location = useLocation()
-  const processId = location.pathname.slice(location.pathname.lastIndexOf('/') + 1)
 
-  React.useEffect(() => {
-    console.log('client list changed: ', process)
-  }, [process])
-
-  React.useEffect(() => {
-    getDetail(processId);
-  }, [])
 
   const getDetail = async (processId) => {
     try {
-      console.log(`${serverUri}/api/process/${processId}`);
       const fetchRes = await fetch(`${serverUri}/api/process/${processId}`, {
         method: 'GET',
         headers: {
@@ -60,72 +62,155 @@ export default function Detail() {
     }
   }
 
+  const handleJobStop = async () => {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const hour = String(currentDate.getHours()).padStart(2, '0');
+    const minute = String(currentDate.getMinutes()).padStart(2, '0');
+    const second = String(currentDate.getSeconds()).padStart(2, '0');
+    
+    const formattedDate = `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+
+    const body = {
+      "keyword": process.keyword,
+      "prePageDown": process.prePageDown,
+      "title": process.title,
+      "method": "findKeyword",
+      "endDate": formattedDate,
+      
+    }
+    
+    try {
+      const fetchRes = await fetch(`${serverUri}/api/process/${processId}`, {
+        method: 'PATCH',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+      })
+      const responseData = await fetchRes.json()
+      if (responseData && responseData.code === 200) {
+        console.log('성공')
+      }
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  const handleEdit = () => {
+    setIsEditing(!isEditing);
+  };
+
+  useEffect(() => {
+    console.log('client list changed: ', process)
+  }, [process])
+
+  useEffect(() => {
+    getDetail(processId);
+  }, [])
+
   return (
     <Card className="mt-10">
       <CardHeader variant="gradient" color="blue" className="mb-8 p-6 flex justify-between items-center">
         <Typography variant="h6" color="white">
           작업 진행 상세
         </Typography>
-        <Button
-          color="gray"
-          // buttonType="link"
-          size="sm"
-          onClick={() => { }} // 추가
-        >
-          작업 중지
-        </Button>
+        <div className="flex items-center">
+          <Button color="cyan" size="sm" onClick={handleEdit} className="px-4 py-2 rounded-md mr-4">
+            {isEditing ? "수정 완료" : "수정"}
+          </Button>
+          <Button
+            color="red"
+            // buttonType="link"
+            size="sm"
+            onClick={handleJobStop} // 추가
+            className="mr-2"
+          >
+            작업 종료
+          </Button>
+        </div>
       </CardHeader>
       <CardBody className="">
-        <div className="flex flex-row flex-wrap">
-
-          <div className="basis-1/2 mb-4">
-            <Typography variant="small" className="font-normal text-blue-gray-500">
+        <div className="grid grid-cols-2 gap-2">
+          <div className="mb-4 flex flex-row items-center">
+            <Typography variant="small" className="font-normal text-blue-gray-500" style={{ width: '9em' }}>
               검색 키워드
             </Typography>
-            <Typography variant="h6" color="blue-gray" className="mt-1 mb-2">
-              {process.keyword ?? ''}
-            </Typography>
+            <Input
+              type="text"
+              value={process?.keyword}
+              onChange={(e) => setProcess({ ...process, keyword: e.target.value })}
+              className="w-full mt-1 px-2 py-1 text-sm"
+              disabled={!isEditing}
+              style={{ gridColumn: "2/3" }} // 자식 요소가 첫 번째 열에 위치하도록 함
+            />
           </div>
-          <div className="basis-1/2 mb-4">
-            <Typography variant="small" className="font-normal text-blue-gray-500">
+          <div className="mb-4 flex flex-row items-center">
+            <Typography variant="small" className="font-normal text-blue-gray-500" style={{ width: '9em' }}>
               찾는 문자열
             </Typography>
-            <Typography variant="h6" color="blue-gray" className="mt-1 mb-2">
-              {process.title ?? ''}
-            </Typography>
+            <Input
+              type="text"
+              value={process?.title}
+              onChange={(e) => setProcess({ ...process, title: e.target.value })}
+              className="w-full mt-1 px-2 py-1 text-sm"
+              disabled={!isEditing}
+              style={{ gridColumn: "2/3" }} // 자식 요소가 첫 번째 열에 위치하도록 함
+            />
           </div>
-
-          <div className="basis-1/2 mb-4">
-            <Typography variant="small" className="font-normal text-blue-gray-500">
+          <div className="mb-4 flex flex-row items-center">
+            <Typography variant="small" className="font-normal text-blue-gray-500" style={{ width: '9em' }}>
               시작시간
             </Typography>
-            <Typography variant="h6" color="blue-gray" className="mt-1 mb-2">
-              {process.createdAt ?? ''}
-            </Typography>
+            <Input
+              type="text"
+              value={process?.createdAt}
+              onChange={(e) => setProcess({ ...process, createdAt: e.target.value })}
+              className="w-full mt-1 px-2 py-1 text-sm"
+              disabled={!isEditing}
+              style={{ gridColumn: "2/3" }} // 자식 요소가 첫 번째 열에 위치하도록 함
+            />
           </div>
-          <div className="basis-1/2 mb-4">
-            <Typography variant="small" className="font-normal text-blue-gray-500">
+          <div className="mb-4 flex flex-row items-center">
+            <Typography variant="small" className="font-normal text-blue-gray-500" style={{ width: '9em' }}>
               종료시간
             </Typography>
-            <Typography variant="h6" color="blue-gray" className="mt-1 mb-2">
-              {process.endDate ?? ''}
-            </Typography>
+            <Input
+              type="text"
+              value={process?.createndDateedAt}
+              onChange={(e) => setProcess({ ...process, createndDateedAt: e.target.value })}
+              className="w-full mt-1 px-2 py-1 text-sm"
+              disabled={!isEditing}
+              style={{ gridColumn: "2/3" }} // 자식 요소가 첫 번째 열에 위치하도록 함
+            />
           </div>
-          <div className="basis-1/2 mb-4">
-            <Typography variant="small" className="font-normal text-blue-gray-500">
+          <div className="mb-4 flex flex-row items-center">
+            <Typography variant="small" className="font-normal text-blue-gray-500" style={{ width: '9em' }}>
               클라이언트 수
             </Typography>
-            <Typography variant="h6" color="blue-gray" className="mt-1 mb-2">
-              {process?.clients?.clientsCount ?? 0}
-            </Typography>
+            <Input
+              type="text"
+              value={process?.clients?.clientsCount}
+              onChange={(e) => setProcess({ ...process?.clients, clientsCount: e.target.value })}
+              className="w-full mt-1 px-2 py-1 text-sm"
+              disabled={!isEditing}
+              style={{ gridColumn: "2/3" }} // 자식 요소가 첫 번째 열에 위치하도록 함
+            />
           </div>
-          <div className="basis-1/2 mb-4">
-            <Typography variant="small" className="font-normal text-blue-gray-500">
+          <div className="mb-4 flex flex-row items-center">
+            <Typography variant="small" className="font-normal text-blue-gray-500" style={{ width: '9em' }}>
               프리페이지 다운
             </Typography>
-            <Typography variant="h6" color="blue-gray" className="mt-1 mb-2">
-              {process.prePagedown ?? 0}
-            </Typography>
+            <Input
+              type="text"
+              value={process?.prePagedown}
+              onChange={(e) => setProcess({ ...process, prePagedown: e.target.value })}
+              className="w-full mt-1 px-2 py-1 text-sm"
+              disabled={!isEditing}
+              style={{ gridColumn: "2/3" }} // 자식 요소가 첫 번째 열에 위치하도록 함
+            />
           </div>
         </div>
         <div className="w-full">
