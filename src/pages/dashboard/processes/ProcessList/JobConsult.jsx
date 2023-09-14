03b1d8e1634ac8bbdc7536b4bf8ec5d758a18215
looter03 <parkjaewoong1203@gmail.com>
2023-09-14 19:38:03 +0900

    change easy paste button

--- src/pages/dashboard/processes/ProcessList/JobConsult.jsx
+++ src/pages/dashboard/processes/ProcessList/JobConsult.jsx
@@ -238,51 +238,17 @@ export function JobConsult(props) {
                     Presets
                   </Typography>
                   <div className=" mb-2">
-                    <span>
-                      망작두
-                    </span>
+                    <span>나침반</span>
                     <Button className="ml-2 p-2" onClick={() => {
-                      setRequestDomain('https://i.imgur.com/yQlObDs.png')
+                      setRequestDomain('https://i.imgur.com/scYOVJF.png')
                     }}>
                       붙여넣기
                     </Button>
                   </div>
-                  <div>
-                    <span>
-                      악귀
-                    </span>
+                  <div className=" mb-2">
+                    <span>우리광고</span>
                     <Button className="ml-2 p-2" onClick={() => {
-                      setRequestDomain('https://i.imgur.com/Hrn8suH.png')
-                    }}>
-                      붙여넣기
-                    </Button>
-                  </div>
-                  <div>
-                    <span>
-                      두목곰
-                    </span>
-                    <Button className="ml-2 p-2" onClick={() => {
-                      setRequestDomain('https://i.imgur.com/g9a7X4i.png')
-                    }}>
-                      붙여넣기
-                    </Button>
-                  </div>
-                  <div>
-                    <span>
-                      독종
-                    </span>
-                    <Button className="ml-2 p-2" onClick={() => {
-                      setRequestDomain('https://i.imgur.com/Da7sa6S.png')
-                    }}>
-                      붙여넣기
-                    </Button>
-                  </div>
-                  <div>
-                    <span>
-                      박경영
-                    </span>
-                    <Button className="ml-2 p-2" onClick={() => {
-                      setRequestDomain('https://i.imgur.com/KcqlEHV.png')
+                      setRequestDomain('https://i.imgur.com/ozFWmwc.png')
                     }}>
                       붙여넣기
                     </Button>
